3e2c8b3f3846537cf70e43aa293f7a0836263b0d
Travis Horn <travis@travishorn.com>
2022-02-06 14:53:27 -0600

    Sign up

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
-import { scrypt, timingSafeEqual } from "node:crypto";
+import { randomBytes, scrypt } from "node:crypto";
 import { Router } from "express";
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import db from "../db.js";
+
+const saltLen = +process.env.SALT_LEN || 64;
+const keyLen = +process.env.KEY_LEN || 64;
 
 const router = Router();
 
@@ -58,6 +61,72 @@ router.post(
 router.post("/log-out", (req, res) => {
   req.logout();
   res.redirect("/");
+});
+
+router.get("/sign-up", (req, res) => {
+  res.render("user/sign-up");
+});
+
+router.post("/sign-up", async (req, res, next) => {
+  if (req.body.username === "") {
+    return next({
+      status: 422,
+      message: "Username cannot be blank. Please enter a username.",
+    });
+  }
+
+  if (req.body.password === "") {
+    return next({
+      status: 422,
+      message: "Password cannot be blank. Please enter a password.",
+    });
+  }
+
+  await db.read();
+
+  const existingUserIx = db.data.users
+    .findIndex(user => user.username === req.body.username);
+  
+  if (existingUserIx !== -1) {
+    return next({
+      status: 409,
+      message: "That username is already taken. Please choose another.",
+    });
+  } 
+
+  if (req.body.password !== req.body.retypePassword) {
+    return next({
+      status: 422,
+      message: "The passwords you entered do not match. Please try again.",
+    });
+  }
+
+  randomBytes(saltLen, (saltErr, saltBuf) => {
+    if (saltErr) return console.error(saltErr);
+    const salt = saltBuf.toString("hex");
+
+    scrypt(req.body.password, salt, keyLen, async (pwHashErr, pwHashBuf) => {
+      if (pwHashErr) return console.error(pwHashErr);
+      const pwHash = pwHashBuf.toString("hex");
+
+      db.data.users.push({
+        username: req.body.username,
+        passwordHash: pwHash,
+        salt,
+      });
+
+      await db.write();
+
+      const user = {
+        username: req.body.username,
+      };
+
+      req.login(user, (loginErr) => {
+        if (loginErr) return next(loginErr);
+        res.redirect("/");
+      });
+    });
+  });
 });
 
 export default router;
